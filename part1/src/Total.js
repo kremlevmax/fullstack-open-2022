98b01c34cbdb89fee6331f39b489f8df715010eb
kremlevmax <kremlevmax1989@gmail.com>
2022-03-21 16:36:56 -0700

    1.5: course information step5

--- part1/src/Total.js
+++ part1/src/Total.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const Total = (props) => {
+  const totalNumber = props.parts.reduce((sum, a) => sum + a.exercises, 0);
+
   return (
     <>
-      <p>Number of exercises {props.numberOfExercises}</p>
+      <p>Number of exercises {totalNumber}</p>
     </>
   );
 };
