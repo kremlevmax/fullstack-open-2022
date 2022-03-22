968c2b2c4c394a31f5a1fc78adfdda377b1d5495
kremlevmax <kremlevmax1989@gmail.com>
2022-03-22 16:17:00 -0700

    1.7: unicafe step2

--- part1/src/Statistics.js
+++ part1/src/Statistics.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = ((good - bad) / (good + neutral + bad)) * 100 || 0;
+  const positive = (good / (good + neutral + bad)) * 100 || 0;
+
   return (
     <>
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {all}</p>
+      <p>Average {average}</p>
+      <p>Positive {positive}%</p>
     </>
   );
 };
