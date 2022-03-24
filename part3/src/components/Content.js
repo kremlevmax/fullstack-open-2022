40ba52a088a7c13d83dcc943c818d0b5e95db775
kremlevmax <kremlevmax1989@gmail.com>
2022-03-24 00:24:12 -0700

    2.2: Course information step7

--- part3/src/components/Content.js
+++ part3/src/components/Content.js
@@ -1,13 +1,20 @@
 import React from "react";
-
 import Part from "./Part";
+import Total from "./Total";
 
 const Content = ({ content }) => {
   const parts = content.map((part) => (
     <Part key={part.id} name={part.name} exercises={part.exercises} />
   ));
 
-  return <>{parts}</>;
+  const total = content.reduce((partialSum, a) => partialSum + a.exercises, 0);
+
+  return (
+    <>
+      {parts}
+      <Total total={total} />
+    </>
+  );
 };
 
 export default Content;
---
+++ part3/src/components/Total.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Total = ({ total }) => {
+  return (
+    <>
+      <b>Total of {total} exercises</b>
+    </>
+  );
+};
+
+export default Total;
