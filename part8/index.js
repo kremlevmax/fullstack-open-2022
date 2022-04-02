bf13b71527b423cffe04f4ae6fce02f3ff86f4c3
kremlevmax <kremlevmax1989@gmail.com>
2022-04-01 18:19:07 -0700

    3.3: Phonebook backend step3

--- part8/index.js
+++ part8/index.js
@@ -37,6 +37,18 @@ app.get("/info", (request, response) => {
   );
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  console.log(person);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
